626fd266e316f8657e4d6a8d9ca110589c902017
Leonardo Gonzalez <99482538+leofgonzalez@users.noreply.github.com>
2024-12-16 14:19:46 -0300

    Add files via upload

---
+++ Reactivated Users.sql
@@ -0,0 +1,33 @@
+WITH daily_activity AS (
+    SELECT 
+        user_id,
+        DATE_TRUNC('day', created_at) AS day
+    FROM donations
+    GROUP BY 1, 2
+)
+
+SELECT 
+    gs.all_days AS day,
+    COUNT(DISTINCT ad.user_id) AS number_of_reactivated_users
+FROM 
+    generate_series(
+        '2023-01-01'::date,
+        '2023-12-31'::date,
+        '1 day'::interval
+    ) AS gs(all_days)
+
+LEFT JOIN daily_activity ad ON gs.all_days = ad.day
+
+LEFT JOIN daily_activity last_week_activity 
+    ON ad.user_id = last_week_activity.user_id
+    AND last_week_activity.day >= gs.all_days - INTERVAL '7 days'
+    AND last_week_activity.day < gs.all_days
+
+LEFT JOIN daily_activity last_month_activity 
+    ON ad.user_id = last_month_activity.user_id
+    AND last_month_activity.day >= gs.all_days - INTERVAL '29 days'
+    AND last_month_activity.day < gs.all_days
+    AND last_month_activity.day NOT BETWEEN gs.all_days - INTERVAL '7 days' AND gs.all_days
+
+WHERE last_week_activity.user_id IS NULL -- Garante que não esteja ativo na última semana
+GROUP BY 1;
